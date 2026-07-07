bc0df733fcc49df78ce111d4b7df82dabd4e6e85
Masoud19993 <masoudkaramidarmarani@gmail.com>
2026-07-07 15:31:04 +0200

    Sauvgarder les tâches avec localStorage

--- app.js
+++ app.js
@@ -2,6 +2,47 @@ const form = document.querySelector("#task-form");
 const input = document.querySelector("#task-input");
 const taskList = document.querySelector("#task-list");
 
+let tasks = [];
+""
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const savedTasks = localStorage.getItem("tasks");
+
+  if (savedTasks) {
+    tasks = JSON.parse(savedTasks);
+  }
+}
+
+function renderTasks() {
+  taskList.innerHTML = "";
+
+  tasks.forEach(function (task) {
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = task.text;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Supprimer";
+
+    deleteButton.addEventListener("click", function () {
+      tasks = tasks.filter(function (item) {
+        return item.id !== task.id;
+      });
+
+      saveTasks();
+      renderTasks();
+    });
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+  });
+}
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
@@ -11,22 +52,18 @@ form.addEventListener("submit", function (event) {
     return;
   }
 
-  const li = document.createElement("li");
+  const task = {
+    id: Date.now(),
+    text: taskText
+  };
 
-  const span = document.createElement("span");
-  span.textContent = taskText;
+  tasks.push(task);
 
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Supprimer";
-
-  deleteButton.addEventListener("click", function () {
-      li.remove();
-    });
-
-  li.appendChild(span);
-  li.appendChild(deleteButton);
-
-  taskList.appendChild(li);
+  saveTasks();
+  renderTasks();
 
   input.value = "";
 });
+
+loadTasks();
+renderTasks();
